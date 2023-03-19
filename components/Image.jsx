178b84494b76a0b27e4f56c9e6f6cb17d8f2225c
naga7 <naga@wishup.co>
2023-03-19 18:10:30 +0530

    #test

--- components/Image.jsx
+++ components/Image.jsx
@@ -4,7 +4,7 @@ export default function image(props) {
     function triggerDownload(){
              var link = document.createElement("a");
 
-
+     
       link.href = props.data.url;
       link.download = `breaking-bad-logo.png`;
       link.click();
@@ -12,6 +12,7 @@ export default function image(props) {
   return (
     <div style={{textAlign: 'center',marginTop : '0',marginBottom:'10px'}}>
       <Button className='my-3' onClick={triggerDownload}>Download</Button>
+      <a href={props.data.url}>see</a>
       <div style={{textAlign:'center',width:'100vw'}}>
       <img src={props.data.url} alt="" style={{objectFit: 'contain',width:'400px',height:'400px'}} />
       </div>
